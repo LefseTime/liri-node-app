a70e821ccf18b1d8546bbd4809c934dfbaefa7f7
LefseTime <36971616+LefseTime@users.noreply.github.com>
2018-07-10 21:09:51 -0500

    split into functions to make do what it says work

--- liri.js
+++ liri.js
@@ -1,13 +1,15 @@
+//Inner Sanctum of the Sacred Writs
 require("dotenv").config();
 
+//Room of Requirements
 let keys = require("./keys.js");
 let request = require("request");
 let fs = require('fs');
 let Twitter = require('twitter');
 let Spotify = require('node-spotify-api');
 
-
-if (process.argv[2] == `my-tweets`) {
+//Function Junction
+function twit() {
     let client = new Twitter(keys.twitter);
 
     let params = { screen_name: 'AcctFunky' };
@@ -22,42 +24,69 @@ if (process.argv[2] == `my-tweets`) {
             console.log(`😿 No cat facts for you! 😿`)
         }
     });
+};
 
+function smoothJamz() {
+    let spotify = new Spotify(keys.spotify);
+}
+
+function ebert() {
+    request("http://www.omdbapi.com/?t=" + movieName + "&y=&plot=short&apikey=trilogy", function (error, response, body) {
+
+        //fix this error handling stuff
+        // if (!response) {
+        //     console.log(`The movie you have selected is not one. Please select one that is.`)
+        // }
+
+        // if (!error && response.statusCode === 200) {
+        //     console.log(body);
+
+        //     if (body.Response == "False") {
+        //         console.log(`The movie you have selected is not one. Please select one that is.`)
+        //     }
+
+        //if some data doesn't exist, nothing shows up...
+        console.log(`🎬 The film ${JSON.parse(body).Title} was released in ${JSON.parse(body).Year}. It has a rating of ${JSON.parse(body).Ratings[0].Value} on IMDB and ${JSON.parse(body).Ratings[1].Value} on Rotten Tomatoes. It was produced in ${JSON.parse(body).Country} in ${JSON.parse(body).Language}. The plot is described as: "${JSON.parse(body).Plot}" Actors in ${JSON.parse(body).Title} include ${JSON.parse(body).Actors}.`);
+
+    });
+}
+
+//If
+if (process.argv[2] == `my-tweets`) {
+    twit();
 };
 
 if (process.argv[2] == `spotify-this-song`) {
-    let spotify = new Spotify(keys.spotify);
-
+    smoothJamz();
 };
 
 if (process.argv[2] == `movie-this`) {
     if (!process.argv[3]) {
-        movieName = "mr nobody"
+        movieName = "the 5000 fingers of dr t"
     }
     else {
         movieName = process.argv.splice(3).join("+");
     }
-    request("http://www.omdbapi.com/?t=" + movieName + "&y=&plot=short&apikey=trilogy", function (error, response, body) {
-
-        // If the request is successful (i.e. if the response status code is 200)
-        if (!error && response.statusCode === 200) {
-
-            // Parse the body of the site and recover just the imdbRating
-            // (Note: The syntax below for parsing isn't obvious. Just spend a few moments dissecting it).
-            console.log(`🎥 The film ${JSON.parse(body).Title} was released in ${JSON.parse(body).Year}. It has a rating of ${JSON.parse(body).Ratings[0].Value} on IMDB and ${JSON.parse(body).Ratings[1].Value} on Rotten Tomatoes. It was produced in ${JSON.parse(body).Country} in ${JSON.parse(body).Language}. The plot is described as: "${JSON.parse(body).Plot}" Actors in ${JSON.parse(body).Title} include ${JSON.parse(body).Actors}. 🎬`);
-
-            //    * Title of the movie.
-            //    * Year the movie came out.
-            //    * IMDB Rating of the movie.
-            //    * Rotten Tomatoes Rating of the movie.
-            //    * Country where the movie was produced.
-            //    * Language of the movie.
-            //    * Plot of the movie.
-            //    * Actors in the movie.
-        }
-    });
+    ebert();
 };
 
 if (process.argv[2] == `do-what-it-says`) {
+    fs.readFile('random.txt', 'utf8', function (error, data) {
+        if (error) {
+            console.log(`Something's kinda messed up. 🤷`)
+        }
+        
+        var dataArr = data.split(",");
 
+        if (dataArr[0] == 'my-tweets') {
+            twit();
+        }
+        if (dataArr[0] == 'spotify-this-song') {
+            smoothJamz();
+        }
+        if (dataArr[0] == 'movie-this') {
+            movieName = dataArr[1];
+            ebert();
+        }
+    })
 };
